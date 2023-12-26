e6837ffdbe3f55ba32dc3c0cefa1072b89216e49
VickyUA <vickyy379@gmail.com>
2023-12-26 19:19:34 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,24 @@ import { nanoid } from 'nanoid';
 export default class App extends Component {
   state = {
     contacts: [],
+    filter: '',
   };
-
-  // contactId = nanoid();
 
   formSubmitHandler = ({ name, number }) => {
     const newContact = { name, number, id: nanoid() };
-    // this.setState(prev => ({ contacts: [...prev.contacts, data] }));
     this.setState(prev => ({ contacts: [...prev.contacts, newContact] }));
     console.log(this.state);
   };
 
+  handleFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
   render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
     return (
       <div
         style={{
@@ -30,8 +36,15 @@ export default class App extends Component {
         <Form getSubmitData={this.formSubmitHandler} />
         <div>
           <h2>Contacts</h2>
-          {this.state.contacts.map(contact => (
-            <li key={contact}>
+          <h3>Find contacts by name</h3>
+          <input
+            type="text"
+            name="filter"
+            value={this.state.filter}
+            onChange={this.handleFilter}
+          />
+          {filteredContacts.map(contact => (
+            <li key={contact.id}>
               {contact.name}: {contact.number}
             </li>
           ))}
